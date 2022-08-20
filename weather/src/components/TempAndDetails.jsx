badb4978cb853a6e83c4df7030db56d91ac17de0
Ayush451 <88429944+Ayush451@users.noreply.github.com>
2022-08-20 13:25:13 +0100

    Created the design and structure

---
+++ weather/src/components/TempAndDetails.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import {
+    UilArrowUp,
+    UilArrowDown,
+    UilTemperature,
+    UilTear,
+    UilWind,
+    UilSun,
+    UilSunset,
+} from '@iconscout/react-unicons'
+
+const TempAndDetails = () => {
+  return (
+    <div>
+        <div className='flex items-center justify-center py-6 text-xl text-cyan-300'>
+            <p>Cloudy</p>
+        </div>
+        <div className='flex flex-row items-center justify-between text-white py-3'>
+            <img src="" alt="" className='w-20'/>
+            <p className='text-5xl'>34°</p>
+            <div className='flex flex-col space-y-2 '>
+                <div className='flex font-light text-sm items-center justify-center'>
+                    <UilTemperature size={18} className='mr-1'/>
+                    Real fell:
+                    <span className='font-medium ml-1'>32°</span>
+                </div>
+
+                <div className='flex font-light text-sm items-center justify-center'>
+                    <UilTear size={18} className='mr-1'/>
+                    Humidity:
+                    <span className='font-medium ml-1'>65%</span>
+                </div>
+
+                <div className='flex font-light text-sm items-center justify-center'>
+                    <UilWind size={18} className='mr-1'/>
+                    Wind:
+                    <span className='font-medium ml-1'>11 km/h</span>
+                </div>
+
+            </div>
+        </div>
+
+        <div className='flex flex-row items-center justify-center space-x-2 text-white text-sm py-3'>
+            <UilSun/>
+            <p className='font-light ml-1'>Rise: <span>06:45 AM</span></p>
+            <p className='font-light'>|</p>
+
+            <UilSunset/>
+            <p className='font-light ml-1'>Set: <span>09:45 PM</span></p>
+            <p className='font-light'>|</p>
+
+            <UilArrowUp/>
+            <p className='font-light ml-1'>Low: <span>45°</span></p>
+
+            <UilArrowDown/>
+            <p className='font-light ml-1'>Low: <span>40°</span></p>
+            
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default TempAndDetails
